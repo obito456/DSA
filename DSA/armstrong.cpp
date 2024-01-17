da4eaf794a19c07f41c0f560ed164ab6acedf191
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-17 23:23:40 +0530

    Update armstrong.cpp

--- DSA/armstrong.cpp
+++ DSA/armstrong.cpp
@@ -2,20 +2,30 @@
 #include<cmath>
 using namespace std;
 
+int countDigits(int num) {
+    int count = 0;
+    while (num > 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
 int main(){
    int n;
    cout<<"enter=";
    cin>>n;
    int sum=0;
    int a=n;
+   int num = countDigits(n);
    while(n>0){
        int last=n%10;
-       sum+=pow(last,3);
+       sum+=pow(last,num);
        n=n/10;
    }
     
     if(sum==a){
-        cout<<sum<<endl;
+        cout<<"yes "<<sum<<endl;
     }
     else{
         cout<<"not"<<endl;
