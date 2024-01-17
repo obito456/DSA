931aaaedf5bb8a8cac0d162c1ac80d61cafe4a9d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-17 23:04:04 +0530

    Update prime.cpp

--- DSA/prime.cpp
+++ DSA/prime.cpp
@@ -2,23 +2,20 @@
 #include<cmath>
 using namespace std;
 
-int main() {
+int main(){
     int n;
-    cout << "Enter a number: ";
-    cin >> n;
-
-    bool flag = 0;
-
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            cout << "Not prime" << endl;
-            flag = 1;
+    cout<<"enter=";
+    cin>>n;
+    bool flag=false;                // Corrected initialization  0
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<"not prime"<<endl;
+            flag=true;               // Corrected assignment   1
             break;
         }
+    }    
+    if(!flag){                     //   1
+        cout<<"prime"<<endl;
     }
-
-    if (flag==0) {
-        cout << "Prime" << endl;
-    }
-
     return 0;
+}
