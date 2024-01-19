ace8e9a39fc388961723e1e49e89639d066043a3
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-19 11:40:42 +0530

    Create 9.palindrome f.cpp

---
+++ DSA/9.palindrome f.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        long reversed = 0;
+        int y = x;
+
+        while (y > 0) {
+            reversed = reversed * 10 + y % 10;
+            y /= 10;
+        }
+
+        return reversed == x;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Test cases
+    int num1 = 121;
+    int num2 = -121;
+    int num3 = 10;
+
+    cout << num1 << " is " << (solution.isPalindrome(num1) ? "" : "not ") << "a palindrome." << endl;
+    cout << num2 << " is " << (solution.isPalindrome(num2) ? "" : "not ") << "a palindrome." << endl;
+    cout << num3 << " is " << (solution.isPalindrome(num3) ? "" : "not ") << "a palindrome." << endl;
+
+    return 0;
+}
